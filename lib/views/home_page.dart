8331f55ed8d0e55a039f9aa86731a335c0c00b78
amanuel <gulilatamanuel@gmail.com>
2024-10-22 22:49:06 +0300

    SplashPage, HomePage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  static String route = 'home-page';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+        body: Container(
+      color: Colors.amber,
+    ));
   }
 }
